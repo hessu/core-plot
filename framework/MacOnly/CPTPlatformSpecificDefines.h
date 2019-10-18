efb0e1d2d7e710c226af8c7b47bdcf4829a4c0e8
Heikki Hannikainen <hessu@hes.iki.fi>
2019-10-18 20:28:55 +0300

    Remove MacOnly directory to make core-plot build for iOS

--- framework/MacOnly/CPTPlatformSpecificDefines.h
+++
@@ -1,5 +0,0 @@
-/// @file
-
-typedef NSImage CPTNativeImage; ///< Platform-native image format.
-typedef NSEvent CPTNativeEvent; ///< Platform-native OS event.
-typedef NSFont  CPTNativeFont;  ///< Platform-native font.
